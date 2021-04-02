508faef62e6d79bde3d80c92d97c81d4f1e3b78e
aaroluo <aaro.luo@gmail.com>
2021-04-02 19:41:35 +0300

    added FutureOr extension

--- lib/src/futureExt.dart
+++ lib/src/futureExt.dart
@@ -2,9 +2,16 @@ import 'dart:async';
 
 import '../rusted.dart';
 
-extension ResultExt<O, E> on Future<Result<O, E>> {
+extension FutureResultExt<O, E> on Future<Result<O, E>> {
   Future<B> foldAsync<B>(B Function(O ok) ok, B Function(E err) err) async {
     final result = await this;
-    return result.fold((okay) => ok(okay), (error) => err(error));
+    return result.fold((value) => ok(value), (error) => err(error));
+  }
+}
+
+extension FutureOrResultExt<O, E> on FutureOr<Result<O, E>> {
+  Future<B> foldAsync<B>(B Function(O ok) ok, B Function(E err) err) async {
+    final result = await this;
+    return result.fold((value) => ok(value), (error) => err(error));
   }
 }
